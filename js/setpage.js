a7726137f926160cb35ea8201607d1410f4a3c3e
xiayuxiaoyan <517594576@qq.com>
2015-06-13 12:15:26 +0800

    微改_耿彩丽
    微改_耿彩丽

---
+++ js/setpage.js
@@ -0,0 +1,99 @@
+var totalpage,pagesize,cpage,count,curcount,outstr; 
+//初始化 
+cpage = 1; 
+totalpage = 56; 
+pagesize = 10; 
+outstr = ""; 
+function gotopage(target) 
+{     
+    cpage = target;        //把页面计数定位到第几页 
+    setpage(); 
+    //reloadpage(target);    //调用显示页面函数显示第几页,这个功能是用在页面内容用ajax载入的情况 
+} 
+function setpage() 
+{ 
+    if(totalpage<=10){        //总页数小于十页 
+        for (count=1;count<=totalpage;count++) 
+        {    if(count!=cpage)  
+            { 
+                outstr = outstr + "<a href='javascript:void(0)' onclick='gotopage("+count+")'>"+count+"</a>";   //不是当前页则创一个连接 
+            }else{ 
+                outstr = outstr + "<span class='current' >"+count+"</span>";   //是当前页则为该数字
+            } 
+        } 
+    } 
+    if(totalpage>10){        //总页数大于十页 
+        if(parseInt((cpage-1)/10) == 0)   
+        {                                 //第1、11、21……页
+            for (count=1;count<=10;count++) 
+            {    if(count!=cpage) 
+                {                                        //不是当前页则创一个连接
+                    outstr = outstr + "<a href='javascript:void(0)' onclick='gotopage("+count+")'>"+count+"</a>"; 
+                }else{ 
+                    outstr = outstr + "<span class='current'>"+count+"</span>"; 
+                }                                       //是当前页则为该数字
+            } 
+            outstr = outstr + "<a href='javascript:void(0)' onclick='gotopage("+count+")'> next </a>"; 
+        } 
+        else if(parseInt((cpage-1)/10) == parseInt(totalpage/10)) // 最后第一轮
+        {                                     
+            outstr = outstr + "<a href='javascript:void(0)' onclick='gotopage("+(parseInt((cpage-1)/10)*10)+")'>previous</a>"; 
+            for (count=parseInt(totalpage/10)*10+1;count<=totalpage;count++) 
+            {    if(count!=cpage) 
+                { 
+                    outstr = outstr + "<a href='javascript:void(0)' onclick='gotopage("+count+")'>"+count+"</a>"; 
+                }else{ 
+                    outstr = outstr + "<span class='current'>"+count+"</span>"; 
+                } 
+            } 
+        } 
+        else 
+        {     
+            outstr = outstr + "<a href='javascript:void(0)' onclick='gotopage("+(parseInt((cpage-1)/10)*10)+")'>previous</a>"; 
+            for (count=parseInt((cpage-1)/10)*10+1;count<=parseInt((cpage-1)/10)*10+10;count++) 
+            {         
+                if(count!=cpage) 
+                { 
+                    outstr = outstr + "<a href='javascript:void(0)' onclick='gotopage("+count+")'>"+count+"</a>"; 
+                }else{ 
+                    outstr = outstr + "<span class='current'>"+count+"</span>"; 
+                } 
+            } 
+            outstr = outstr + "<a href='javascript:void(0)' onclick='gotopage("+count+")'> next </a>"; 
+        } 
+    }     
+    document.getElementById("setpage").innerHTML = "<div id='setpage'><span id='info'>共"+totalpage+"页|第"+cpage+"页<\/span>" + outstr + "<\/div>"; 
+    outstr = ""; 
+} 
+
+
+
+var xmlhttp;
+function loadXMLDoc(url,cfunc)
+{
+   if (window.XMLHttpRequest)
+      {// code for IE7+, Firefox, Chrome, Opera, Safari
+         xmlhttp=new XMLHttpRequest();
+      }
+   else
+       {// code for IE6, IE5
+          xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+        }
+   xmlhttp.onreadystatechange=cfunc;
+   xmlhttp.open("GET",url,true);
+   xmlhttp.send();
+}
+function reloadpage(target)
+   {
+     loadXMLDoc("/ajax/test1.txt",function()
+        {
+          if (xmlhttp.readyState==4 && xmlhttp.status==200)
+             {
+                  document.getElementById("XXX").innerHTML=xmlhttp.responseText;
+             }
+        });
+   }
+
+
+setpage();    //调用分页 
+//--> 
